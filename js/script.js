820b5579107a662f2cc3e3b43848d7c1978cee82
diver <madciborg@gmail.com>
2023-03-30 14:22:37 +0600

    first console code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const numberOfFiles = +prompt('Сколько фильмов вы посмотрели', '3');
+const personalMovieDB = {
+    count: numberOfFiles,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let movieName = '',
+    movieRating = 0;
+
+    movieName = prompt('Один из последних просмотренных фильмов?', '');
+    movieRating = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[movieName] = movieRating;
+
+    movieName = prompt('Один из последних просмотренных фильмов?', '');
+    movieRating = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[movieName] = movieRating;
+
+    console.log(personalMovieDB);
